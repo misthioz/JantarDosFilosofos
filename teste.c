36dd6cdda2658209a380751725fde89ce52f0fbc
Arthur <arthurferreira.dev@gmail.com>
2023-03-25 13:06:02 -0300

    teste 2

--- teste.c
+++ teste.c
@@ -3,27 +3,84 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<semaphore.h>
+#include <pthread.h>
 
 #define COMENDO 2
 #define FOME 1
 #define PENSANDO 0
 
-sem_t garfos[5];
-sem_t mutex;
-int v_filosofos[5] = {1, 2, 3, 4, 5}; 
+sem_t semFilosofos[5];
+sem_t mutex; 
 int estadoFilosofo[5];
 
-//funcoes para facilitar recuperar os vizinhos de um Filosofo i
-int vizinhoEsquerdo(int i){
+//funcoes para facilitar recuperar os vizinhos/garfos  de um Filosofo i
+int esquerdo(int i){
 	return (i+4)%5;
 }
 
-int vizinhoDireito(int i){
+int direito(int i){
 	return (i+1)%5
 }
 
-//verificando os estados dos viznhos de um Filosofo i
-void 
+//verifica os estados dos vizinhos e do  Filosofo numFilosofo - se ele estiver com fome e eles nao estiverem comendo ele pode comer
+void verifica(int numFilosofo){
+	if(estadoFilosofos[numFilosofo] == FOME){
+		if(estadoFilosofos[esquerdo(numFilosofo)] != COMENDO && estadoFilosofos[direito(numFilosofo)] != COMENDO){
+			//setando o estado do filosofo
+			estadoFilosfos[numFilosofo] = COMENDO;
+			printf("\nO filosofo %d está comendo.",numFilosofo);
+			//espera
+			sleep(1);
+			//sinaliza qye terminou de comer
+			sem_post(&semFilosofos[numFilosofo]);
+
+		}
+			
+	}
+}
+
+void pegarGarfo(int numFilosofo){
+	//seta estado como fome
+	estadoFilosofos[numFilosofo] = FOME;
+
+	//entra na regiao critica, outros filosos nao podem pegar o garfo ao mesmo tempo
+	sem_wait(&mutex);
+
+	//verifica se ele pode comer (se esta com fome e os vizinhos nao estao comendo), se sim, dentro da funcao verifica() muda seu estado para comendo
+	//se nao, ele espera ate que o filosofo que esta comendo sinalize que acabou de comer
+	verifica(numFilosofo);
+
+	//sai da regiao critica, agora outros filosofos podem tentar pegar o garfo
+	sem_post(&mutex);
+
+	//volta para 0
+	sem_wait(&semFilosofos[numFilosofo]);
+}
+
+void soltarGarfo(int numFilosofo){
+	//entra na regiao critica, outros filosofos nao podem interagir com o garfo ate este filosofo soltar
+	sem_wait(&mutex);	
+
+	//quando o filosofo termina de comer, volta para o estado pensando
+	estadoFilosofos[numFilosofo] = PENSANDO;
+
+	//chama a funcao verifica() para os vizinhos e testa se eles podem começar a comer
+	verifica(esquerdo(numFilosofo));
+	verifica(direito(numFilosofo));
+
+	//sai da regiao critica
+	sem_post(&mutex);
+}
+
+
+void filosofos(){
+	//while true
+	//num fiilosofo aleatorio?
+	//sleep
+	//fome
+	//sleep
+	//comer
+}
 
 int main(){
 	//inicializando o vetor de semaforos (garfos)
